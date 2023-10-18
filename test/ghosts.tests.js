d74695b7e7c97ee7642eef397b4f446abc64f192
Andre Staltz <andre@staltz.com>
2023-10-18 17:04:50 +0300

    add ghosts API

---
+++ test/ghosts.tests.js
@@ -0,0 +1,56 @@
+const test = require('node:test')
+const assert = require('node:assert')
+const path = require('node:path')
+const os = require('node:os')
+const p = require('node:util').promisify
+const rimraf = require('rimraf')
+const SecretStack = require('secret-stack')
+const caps = require('ppppp-caps')
+const Keypair = require('ppppp-keypair')
+
+const DIR = path.join(os.tmpdir(), 'ppppp-db-ghosts')
+rimraf.sync(DIR)
+
+const keypair = Keypair.generate('ed25519', 'alice')
+test('ghosts.add, ghosts.get, ghosts.getMinDepth', async (t) => {
+  const peer = SecretStack({ appKey: caps.shse })
+    .use(require('../lib'))
+    .use(require('ssb-box'))
+    .call(null, { keypair, path: DIR })
+
+  await peer.db.loaded()
+
+  const account = await p(peer.db.account.create)({ domain: 'person' })
+  const MAX = 5
+
+  let msgIDs = []
+  for (let i = 0; i < 10; i++) {
+    const rec = await p(peer.db.feed.publish)({
+      account,
+      domain: 'post',
+      data: { text: 'hello ' + i },
+    })
+    msgIDs.push(rec.id)
+  }
+  const feedID = peer.db.feed.getID(account, 'post')
+
+  await p(peer.db.ghosts.add)({ msg: msgIDs[0], tangle: feedID, max: MAX })
+  await p(peer.db.ghosts.add)({ msg: msgIDs[1], tangle: feedID, max: MAX })
+  await p(peer.db.ghosts.add)({ msg: msgIDs[2], tangle: feedID, max: MAX })
+  await p(peer.db.ghosts.add)({ msg: msgIDs[3], tangle: feedID, max: MAX })
+  await p(peer.db.ghosts.add)({ msg: msgIDs[4], tangle: feedID, max: MAX })
+
+  const ghostsA = await p(peer.db.ghosts.get)(feedID)
+  assert.deepEqual(ghostsA, msgIDs.slice(0, 5), 'ghosts so far')
+  const depthA = await p(peer.db.ghosts.getMinDepth)(feedID)
+  assert.equal(depthA, 1, 'min depth so far')
+
+  await p(peer.db.ghosts.add)({ msg: msgIDs[5], tangle: feedID, max: MAX })
+
+  const ghostsB = await p(peer.db.ghosts.get)(feedID)
+  assert.deepEqual(ghostsB, msgIDs.slice(1, 6), 'ghosts so far')
+  const depthB = await p(peer.db.ghosts.getMinDepth)(feedID)
+  assert.equal(depthB, 2, 'min depth so far')
+
+  await p(peer.close)(true)
+})
