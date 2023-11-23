a84dd297a560f6da1b5de5fb5f074889ceb3af46
Andre Staltz <andre@staltz.com>
2023-11-23 15:04:40 +0200

    log: implement compaction (sift-clone and rename)

---
+++ test/log/compact.test.js
@@ -0,0 +1,162 @@
+const test = require('node:test')
+const assert = require('node:assert')
+const p = require('node:util').promisify
+const Log = require('../../lib/log')
+
+test('Log compaction', async (t) => {
+  await t.test('compact a log that does not have holes', async (t) => {
+    const file = '/tmp/ppppp-db-log-compaction-test-' + Date.now() + '.log'
+    const log = Log(file, { blockSize: 15 })
+
+    const stats = await p(log.stats)()
+    assert.equal(stats.totalBytes, 0, 'stats.totalBytes (1)')
+    assert.equal(stats.deletedBytes, 0, 'stats.deletedBytes (1)')
+
+    const buf1 = Buffer.from('first')
+    const buf2 = Buffer.from('second')
+
+    const offset1 = await p(log.append)(buf1)
+    const offset2 = await p(log.append)(buf2)
+    await p(log.onDrain)()
+    assert('append two records')
+
+    const stats2 = await p(log.stats)()
+    assert.equal(stats2.totalBytes, 15, 'stats.totalBytes (2)')
+    assert.equal(stats2.deletedBytes, 0, 'stats.deletedBytes (2)')
+
+    const progressArr = []
+    log.compactionProgress((stats) => {
+      progressArr.push(stats)
+    })
+
+    await p(log.compact)()
+    await p(log.onDrain)()
+
+    assert.deepEqual(
+      progressArr,
+      [
+        { percent: 0, done: false },
+        { percent: 1, done: true, sizeDiff: 0, holesFound: 0 },
+      ],
+      'progress events'
+    )
+
+    await new Promise((resolve, reject) => {
+      const arr = []
+      log.scan(
+        (offset, data, size) => {
+          arr.push(data)
+        },
+        (err) => {
+          if (err) return reject(err)
+          assert.deepEqual(arr, [buf1, buf2], 'both records exist')
+          resolve()
+        }
+      )
+    })
+
+    await p(log.close)()
+  })
+
+  await t.test('delete first record, compact, stream', async (t) => {
+    const file = '/tmp/ppppp-db-log-compaction-test-' + Date.now() + '.log'
+    const log = Log(file, { blockSize: 15 })
+
+    const buf1 = Buffer.from('first')
+    const buf2 = Buffer.from('second')
+
+    const progressArr = []
+    log.compactionProgress((stats) => {
+      progressArr.push(stats)
+    })
+
+    const offset1 = await p(log.append)(buf1)
+    const offset2 = await p(log.append)(buf2)
+    await p(log.onDrain)()
+    assert('append two records')
+
+    await p(log.del)(offset1)
+    await p(log.onOverwritesFlushed)()
+    assert('delete first record')
+
+    await p(log.compact)()
+    await p(log.onDrain)()
+
+    assert.deepEqual(
+      progressArr,
+      [
+        { percent: 0, done: false },
+        { percent: 1, done: true, sizeDiff: 5, holesFound: 1 },
+      ],
+      'progress events'
+    )
+
+    await new Promise((resolve, reject) => {
+      const arr = []
+      log.scan(
+        (offset, data, size) => {
+          arr.push(data)
+        },
+        (err) => {
+          if (err) return reject(err)
+          assert.deepEqual(arr, [buf2], 'only second record exists')
+          resolve()
+        }
+      )
+    })
+
+    await p(log.close)()
+  })
+
+  await t.test('delete last record, compact, stream', async (t) => {
+    const file = '/tmp/ppppp-db-log-compaction-test-' + Date.now() + '.log'
+    const log = Log(file, { blockSize: 15 })
+
+    const buf1 = Buffer.from('first')
+    const buf2 = Buffer.from('second')
+    const buf3 = Buffer.from('third')
+
+    const offset1 = await p(log.append)(buf1)
+    const offset2 = await p(log.append)(buf2)
+    const offset3 = await p(log.append)(buf3)
+    await p(log.onDrain)()
+    assert('append three records')
+
+    await p(log.del)(offset3)
+    await p(log.onOverwritesFlushed)()
+    assert('delete third record')
+
+    await new Promise((resolve, reject) => {
+      const arr = []
+      log.scan(
+        (offset, data, size) => {
+          arr.push(data)
+        },
+        (err) => {
+          if (err) return reject(err)
+          assert.deepEqual(arr, [buf1, buf2, null], 'all blocks')
+          resolve()
+        }
+      )
+    })
+
+    await p(log.compact)()
+    await p(log.onDrain)()
+
+    await new Promise((resolve, reject) => {
+      const arr = []
+      log.scan(
+        (offset, data, size) => {
+          arr.push(data)
+        },
+        (err) => {
+          if (err) return reject(err)
+          assert.deepEqual(arr, [buf1, buf2], 'last block truncated away')
+          resolve()
+        }
+      )
+    })
+
+    await p(log.close)()
+  })
+})
